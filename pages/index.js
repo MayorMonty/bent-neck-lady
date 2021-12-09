ebc5cb237b39c7ea2175a49e356649cc6c62fa76
Brendan McGuire <brendan@bren.app>
2021-12-09 13:18:33 -0500

    Move compare to index

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,46 @@
 import Head from "next/head";
-import { useScrollPosition } from "@n8tb1t/use-scroll-position";
 import { useWindowSize } from "../lib/hooks";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-export default function Home() {
-  const [offset, setOffset] = useState(0);
-  const { height } = useWindowSize();
+const images = {
+  "Adult Nellie": "/compare/AdultNellie.jpg",
+  Luke: "/compare/Luke.jpg",
+};
 
-  useScrollPosition(({ currPos: position }) => {
-    setOffset(position.y);
-    console.log(position.y);
-  });
+export default function Compare() {
+  const bar = useRef(null);
+  const { width } = useWindowSize();
+  const [moving, setMoving] = useState(false);
+  const [left, setLeft] = useState(width / 2);
+  const [leftImage, setLeftImage] = useState("/compare/AdultNellie.jpg");
+  const [rightImage, setRightImage] = useState("/compare/Luke.jpg");
+
+  const style = {
+    bar: {
+      transform: `translateX(${left}px)`,
+    },
+    left: {
+      width: left,
+      backgroundImage: `url(${leftImage})`,
+      backgroundSize: "100vw 100vh",
+    },
+    right: {
+      backgroundImage: `url(${rightImage})`,
+      backgroundSize: "100vw 100vh",
+      backgroundPositionX: `${width - left}px`,
+    },
+  };
+
+  function onMouseMove(e) {
+    if (!moving) return;
+    e.preventDefault();
+    setLeft(
+      Math.max(
+        width / 5,
+        Math.min((4 * width) / 5, e.clientX + bar.current.offsetLeft)
+      )
+    );
+  }
 
   return (
     <div className="min-h-screen bg-black">
@@ -18,37 +48,37 @@ export default function Home() {
         <title>Who Is The Bent Neck Lady?</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header
-        className="bg-hero-pattern bg-cover bg-center flex flex-col items-center justify-end"
-        style={{ height: Math.min(height - offset, 1.75 * height) }}
-      >
-        <h1
-          className="text-white text-center text-4xl mb-24"
-          style={{ opacity: Math.max(1 + offset / height, 0) }}
-        >
-          Who Is The Bent Neck Lady?
-        </h1>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          className="text-white w-8 h-8 mb-24 animate-bounce"
-          style={{ opacity: Math.max(1 + offset / height, 0) }}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M19 9l-7 7-7-7"
-          />
-        </svg>
-      </header>
-      <main className="min-h-screen bg-black p-6 md:w-2/3 sm:w-full m-auto mt-12">
-        <p class="text-white text-opacity-90">
-          In Mike Flanagan's Netflix series, <em>The Haunting of Hill House</em>, 
-        </p>
+      <main className="relative max-h-screen" onMouseMove={onMouseMove}>
+        <div
+          className="absolute w-2 h-full bg-black bg-opacity-75 cursor-move"
+          style={style.bar}
+          ref={bar}
+          onMouseDown={() => setMoving(true)}
+          onMouseUp={() => setMoving(false)}
+        ></div>
+        <section className="flex h-screen">
+          <div className="left bg-green-500 h-screen" style={style.left}></div>
+          <div
+            className="right bg-red-500 h-screen flex-1"
+            style={style.right}
+          ></div>
+        </section>
+        <nav className="absolute left-0 w-screen bottom-0 h-24 bg-black flex items-center justify-between px-12">
+          <label className="text-3xl block text-left text-white bg-black cursor-pointer" htmlFor="left">
+            <select class="form-select block w-full mt-1 bg-black bg-opacity-60" id="left" onChange={value => setLeftImage(value)}>
+              {Object.entries(images).map(([name, src]) => (
+                <option key={name} value={src}>{name}</option> 
+              ))}
+            </select>
+          </label>
+          <label className="text-3xl block text-left text-white bg-black cursor-pointer" htmlFor="right">
+            <select class="form-select block w-full mt-1 bg-black bg-opacity-60" id="right" onChange={value => setRightImage(value)}>
+            {Object.entries(images).map(([name, src]) => (
+                <option key={name} value={src}>{name}</option> 
+              ))}
+            </select>
+          </label>
+        </nav>
       </main>
     </div>
   );
